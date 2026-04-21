58aac8184918f496748ed55981d85c1c95f25ad2
realwitzwendy-gif <realwitzwendy@gmail.com>
2026-04-21 17:15:18 +0200

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+import Header from '../components/Header'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
